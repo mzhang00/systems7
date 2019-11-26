5ea6bf5e171108b5d2a5209cde8990162cc3c9e1
Michael Zhang <mzhang00@stuy.edu>
2019-11-26 11:46:48 -0500

    Add stuff

--- systems7.c
+++ systems7.c
@@ -14,5 +14,11 @@ int main(){
   printf("hello\n");
   char test[] = "hello";
   printf("%s\n", test);
+  char line[100] = "woah this is cool";
+  char *s1 = line;
+  while (s1 != NULL){
+    printf("[%s]\n", strsep( &s1, " " ));
+    printf("[%s]\n", s1);
+  }
   return 0;
 }
